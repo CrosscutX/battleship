78e734ee825029e6e00504b397b6cec94ed4750d
Crosscut <crosscutcode@gmail.com>
2022-12-10 15:23:18 -0500

    Start styling/implementing game page

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 "use strict";
 import { Ship, Gameboard, Player } from "./battle.js";
 //GAME START--------------------------------------------------------------
-const namePage = document.querySelector(".name-screen");
-const restartBtn = document.querySelector(".restart-button");
-const startBtn = document.querySelector(".start-button");
-console.log("HI");
-restartBtn.addEventListener("click", () => {
-  location.reload();
-});
+const playerName = document.querySelector(".name");
+
 gameLoop();
+//Controls the flow of the game, from start to finish
 function gameLoop() {
   gameStart();
 }
 
 function gameStart() {
-  console.log("gamestart is working");
+  const namePage = document.querySelector(".name-screen");
+  const gamePage = document.querySelector(".game-screen");
+  const restartBtn = document.querySelector(".restart-button");
+  const startBtn = document.querySelector(".start-button");
+
+  restartBtn.addEventListener("click", () => {
+    location.reload();
+  });
+
+  startBtn.addEventListener("click", () => {
+    namePage.style.display = "none";
+    gamePage.style.display = "flex";
+    playerName = document.querySelector(".name");
+  });
 }
 
 function gameEnd(player) {
