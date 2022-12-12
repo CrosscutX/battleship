470cc39f06afc91d92f35a246c1f10480a5b0b0e
Crosscut <crosscutcode@gmail.com>
2022-12-12 16:47:14 -0500

    Empty names update properly

--- main.js
+++ main.js
@@ -2,12 +2,13 @@
 import { Ship, Gameboard, Player } from "./battle.js";
 //GAME START--------------------------------------------------------------
 let coordinates = [null, null];
-let infoText = document.querySelector(".info-text");
 let playerName = document.querySelector(".name");
+const infoText = document.querySelector(".info-text");
 const namePage = document.querySelector(".name-screen");
 const gamePage = document.querySelector(".game-screen");
 const restartBtn = document.querySelector(".restart-button");
 const startBtn = document.querySelector(".start-button");
+const playerBoardName = document.querySelector(".left-container h2");
 
 gameLoop();
 //Controls the flow of the game, from start to finish
@@ -21,9 +22,16 @@ function gameStart() {
   });
 
   startBtn.addEventListener("click", () => {
+    //Reveal the game page, get the value on the textbox and display it above
+    //the player's ship. Info text is applied.
     namePage.style.display = "none";
     gamePage.style.display = "flex";
-    playerName = document.querySelector(".name");
+    playerName = document.querySelector(".name").value;
+    if (!playerName) {
+      playerName = "Commander";
+    }
+
+    playerBoardName.textContent = playerName + "'s" + " Fleet";
     infoText.textContent = "Place Your Ships";
   });
 }
